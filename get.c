a3b967d3464c30e7be40102149544bfd73c5488d
Bukola Omotoso <bukola.omotoso@andela.com>
2020-03-04 23:08:33 +0000

    deleted unwanted branch

--- get.c
+++
@@ -1,7 +0,0 @@
-#include "comms.h"
-int main(){
-	char* source;
-	char* dest;
-	get(dest, source, 2, 0);
-	printf("Data fetched from memory externally %s\n", dest);
-}
--- put.c
+++
@@ -1,8 +0,0 @@
-#include "comms.h"
-#include <stdio.h>
-int main(int argc, char **argv){
-	char* dest;
-	char* source = argv[1];
-	put(dest, source, 7, 0);
-	printf("Data written in memory externally: %s\n", dest);
-}
